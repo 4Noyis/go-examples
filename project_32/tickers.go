d7570dd6cdb3a1be004b614992cd50a47cef9ca7
4Noyis <berenalpb@outlook.com>
2024-10-16 01:13:28 +0300

    31-32

---
+++ project_32/tickers.go
@@ -0,0 +1,38 @@
+package main
+
+/*
+Timers are for when you want to do something once in the future -
+Tickers are for when you want to do something repeatedly at regular intervals.
+*/
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	ticker := time.NewTicker(500 * time.Millisecond)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				fmt.Println("Tick at", t)
+			}
+		}
+	}()
+
+	/*
+		  Tickers can be stopped like timers.
+		Once a ticker is stopped it won’t receive any more values on its channel.
+		We’ll stop ours after 1600ms.
+	*/
+	time.Sleep(1600 * time.Millisecond)
+	ticker.Stop()
+	done <- true
+	fmt.Println("Ticker stopped")
+
+}
